104cdd4d78f9ef373d7022b926d7cdf04b447e31
Katarzyna Kasprzak <kasia.kasprzak18@gmail.com>
2015-10-25 20:06:04 +0100

    improv: add proper regex

--- lib/templates/nukomeet_template.sh
+++ lib/templates/nukomeet_template.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # regex to validate in commit msg
-commit_regex='(wap-[0-9]+|merge)'
-error_msg="Aborting commit. Your commit message is missing either a JIRA Issue ('WAP-1111') or 'Merge'"
+commit_regex='(/feature:|improv:|fix:|docs:|style:|test:/[a-z\s]+/)'
+error_msg="Aborting commit. Your commit message is missing a key words."
 
 if ! grep -iqE "$commit_regex" "$1"; then
     echo "$error_msg" >&2
